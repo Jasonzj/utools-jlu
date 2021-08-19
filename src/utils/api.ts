470f1a24b87322e3f98053809e5059927e6feeb9
Jasonzj <925645402@qq.com>
2021-08-19 20:53:31 +0800

    update

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,23 +1,22 @@
 import got from 'got'
 
 const api = {
-  authServer: 'https://authserver.jluzh.edu.cn/cas',
-  login: 'https://authserver.jluzh.edu.cn/cas/login',
-  jwLogin: 'http://jw.jluzh.edu.cn/xtgl/login_slogin.html',
-  wlkcLogin: 'http://wlkc.jluzh.edu.cn/meol/loginCheck.do',
-  mobileLogin: 'https://mucp.jluzh.edu.cn/_ids_mobile/login18_9',
-  serialNoLogin: 'https://mucp.jluzh.edu.cn/_ids_mobile/serialNoLogin18_9',
-  electricity: 'https://diankong.jluzh.edu.cn/App/index.aspx',
-  getElectricityData: 'https://diankong.jluzh.edu.cn/App/DBGetPost.ashx',
+  authServer: 'https://authserver.zcst.edu.cn/cas',
+  login: 'https://authserver.zcst.edu.cn/cas/login',
+  jwLogin: 'http://jw.zcst.edu.cn/xtgl/login_slogin.html',
+  wlkcLogin: 'http://wlkc.zcst.edu.cn/meol/loginCheck.do',
+  mobileLogin: 'https://mucp.zcst.edu.cn/_ids_mobile/login18_9',
+  serialNoLogin: 'https://mucp.zcst.edu.cn/_ids_mobile/serialNoLogin18_9',
+  electricity: 'https://diankong.zcst.edu.cn/App/index.aspx',
+  getElectricityData: 'https://diankong.zcst.edu.cn/App/DBGetPost.ashx',
   water:
-    'https://mapp.jluzh.edu.cn/_web/_apps/lightapp/waterCharges/mobile/permit/app/pub/index.jsp',
-  getWaterData: 'https://mapp.jluzh.edu.cn/_web/_lightapp/watermanage/list.rst',
-  getAssignmentData:
-    'http://wlkc.jluzh.edu.cn/meol/welcomepage/student/interaction_reminder_v8.jsp',
-  switchCourse: 'http://wlkc.jluzh.edu.cn/meol/lesson/enter_course.jsp',
+    'https://mapp.zcst.edu.cn/_web/_apps/lightapp/waterCharges/mobile/permit/app/pub/index.jsp',
+  getWaterData: 'https://mapp.zcst.edu.cn/_web/_lightapp/watermanage/list.rst',
+  getAssignmentData: 'http://wlkc.zcst.edu.cn/meol/welcomepage/student/interaction_reminder_v8.jsp',
+  switchCourse: 'http://wlkc.zcst.edu.cn/meol/lesson/enter_course.jsp',
   getAssignmentDetailData:
-    'http://wlkc.jluzh.edu.cn/meol/common/hw/student/hwtask.jsp?tagbug=client&strStyle=new03',
-  getScoreData: 'http://jw.jluzh.edu.cn/cjcx/cjcx_cxDgXscj.html?doType=query',
+    'http://wlkc.zcst.edu.cn/meol/common/hw/student/hwtask.jsp?tagbug=client&strStyle=new03',
+  getScoreData: 'http://jw.zcst.edu.cn/cjcx/cjcx_cxDgXscj.html?doType=query',
 }
 
 const headers = {
